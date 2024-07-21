5137df78198b212319882bb321cc48b637f21a3c
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-07-21 20:26:12 +0530

    feat: add install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+# !/bin/sh
+
+set -e
+DEBIAN_FRONTEND=noninteractive
+
+# Install docker if not installed
+if ! [ -x "$(command -v docker)" ]; then
+    echo 'Error: docker is not installed.' >&2
+    echo 'Installing docker...'
+    curl -fsSL https://get.docker.com | sh
+    echo 'Docker installed successfully.'
+else
+    echo 'Docker is already installed.'
+fi
+
+# If ~/agent.key file does not exist, create it
+if [ ! -f ~/agent.key ]; then
+    echo "Generating agent key..."
+    RANDOM_STRING=$(head -c 256 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9')
+    echo $RANDOM_STRING > ~/agent.key
+    echo "Agent key generated successfully."
+fi
+
+# Read the agent key
+AGENT_TOKEN=$(cat ~/agent.key)
+
+# Pull the image
+docker pull ghcr.io/sturtle-security/labagent:latest
+# Generate the config
+microk8s config > ~/kubeconfig
+# Delete service if already exists
+docker rm -f labagent || true
+# Run the container
+docker run -d --name labagent -e TOKEN=$AGENT_TOKEN -v ~/kubeconfig:/app/kubeconfig.yaml ghcr.io/sturtle-security/labagent:latest
